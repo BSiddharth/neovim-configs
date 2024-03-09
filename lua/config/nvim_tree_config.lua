86b501973a2c50068bd45609174b420a67c06978
Sid <siddharthbisht99@gmail.com>
2024-03-10 00:32:07 +0530

    nvim tree focus + diagnostics

--- lua/config/nvim_tree_config.lua
+++ lua/config/nvim_tree_config.lua
@@ -1,2 +1,7 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	diagnostics = {
+		enable = true,
+	},
+	update_focused_file = { enable = true }
+})
 vim.keymap.set("n", "<C-n>", "<cmd> NvimTreeToggle <CR>", { desc = "Toggle nvimtree" })
