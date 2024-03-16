92f043c2c6f02d6c386c866b75b4d36f58fcc1f1
Sid <siddharthbisht99@gmail.com>
2024-03-16 13:30:07 +0530

    do not hide files that are in .gitignore

--- lua/config/nvim_tree_config.lua
+++ lua/config/nvim_tree_config.lua
@@ -2,6 +2,9 @@ require("nvim-tree").setup({
 	diagnostics = {
 		enable = true,
 	},
+	git = {
+		ignore = false,
+	},
 	update_focused_file = { enable = true }
 })
 vim.keymap.set("n", "<C-n>", "<cmd> NvimTreeToggle <CR>", { desc = "Toggle nvimtree" })
