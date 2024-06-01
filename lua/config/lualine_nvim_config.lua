f9cf453bb7a3751e69c8b696087467dddd692066
Sid <siddharthbisht99@gmail.com>
2024-06-01 19:48:02 +0530

    Add lsp server info in lua line

--- lua/config/lualine_nvim_config.lua
+++ lua/config/lualine_nvim_config.lua
@@ -1,5 +1,24 @@
+-- LSP clients attached to buffer
+local clients_lsp = function()
+	local bufnr = vim.api.nvim_get_current_buf()
+
+	local clients = vim.lsp.buf_get_clients(bufnr)
+	if next(clients) == nil then
+		return ""
+	end
+
+	local c = {}
+	for _, client in pairs(clients) do
+		table.insert(c, client.name)
+	end
+	return "\u{f085} " .. table.concat(c, "|")
+end
+
 require("lualine").setup({
 	options = {
-		disabled_filetypes = { "dashboard" } -- to hide lualine when `:q[uit]` form config
-	}
+		disabled_filetypes = { "dashboard" }, -- to hide lualine when `:q[uit]` form config
+	},
+	sections = {
+		lualine_x = { "encoding", clients_lsp, "filetype" },
+	},
 })
