745b797aa9b42cde9ab25a7b335fd05fd30f5109
Sid <siddharthbisht99@gmail.com>
2025-05-25 12:19:04 +0200

    Cleaning and Organizing

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -332,13 +332,7 @@ require("lazy").setup({
 		{
 			"folke/lazydev.nvim",
 			ft = "lua", -- only load on lua files
-			opts = {
-				library = {
-					-- See the configuration section for more details
-					-- Load luvit types when the `vim.uv` word is found
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-				},
-			},
+			opts = require("config.lazydev_config"),
 		},
 	},
 
