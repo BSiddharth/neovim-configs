05b8b3ff34129bf0fd9742a17da3fded30f5a5aa
Sid <siddharthbisht99@gmail.com>
2025-05-25 16:58:27 +0200

    Added cmdline as source

--- lua/config/blink_cmp_config.lua
+++ lua/config/blink_cmp_config.lua
@@ -31,7 +31,7 @@ return {
 	-- Default list of enabled providers defined so that you can extend it
 	-- elsewhere in your config, without redefining it, due to `opts_extend`
 	sources = {
-		default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+		default = { "lazydev", "lsp", "path", "snippets", "buffer", "cmdline" },
 		-- optional blink completion source for require statements and module annotations
 		providers = {
 			lazydev = {
