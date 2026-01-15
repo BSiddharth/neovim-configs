c03717097c1060894230d47d837441fd6e3759ec
Sid <siddharthbisht99@gmail.com>
2026-01-15 16:45:27 +0100

    replace ascii art; old one is not readable

--- lua/auto_cmd.lua
+++ lua/auto_cmd.lua
@@ -2,20 +2,22 @@
 
  set custom auto_cmd here
 
-    .------..------..------..------..------..------..------..------..------..------..------..------.
-   |A.--. ||U.--. ||T.--. ||O.--. ||_.--. ||C.--. ||M.--. ||D.--. ||..--. ||L.--. ||U.--. ||A.--. |
-  | (\/) || (\/) || :/\: || :/\: || :/\: || :/\: || (\/) || :/\: || :(): || :/\: || (\/) || (\/) |
- | :\/: || :\/: || (__) || :\/: || :\/: || :\/: || :\/: || (__) || ()() || (__) || :\/: || :\/: |
-| '--'A|| '--'U|| '--'T|| '--'O|| '--'_|| '--'C|| '--'M|| '--'D|| '--'.|| '--'L|| '--'U|| '--'A|
-`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'
-
+                            o                                                                o
+                           <|>                                                              <|>
+                           < >                                                              < \
+    o__ __o/   o       o    |        o__ __o                 __o__  \o__ __o__ __o     o__ __o/
+   /v     |   <|>     <|>   o__/_   /v     v\               />  \    |     |     |>   /v     |
+  />     / \  < >     < >   |      />       <\            o/        / \   / \   / \  />     / \
+  \      \o/   |       |    |      \         /           <|         \o/   \o/   \o/  \      \o/
+   o      |    o       o    o       o       o             \\         |     |     |    o      |
+   <\__  / \   <\__ __/>    <\__    <\__ __/>   ____o__    _\o__</  / \   / \   / \   <\__  / \
+                                                 /   \
 
 ]]
 
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
