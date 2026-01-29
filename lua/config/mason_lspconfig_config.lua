e89c8ed1d5bcdba0fce19645a8cd3997001276b5
Sid <siddharthbisht99@gmail.com>
2026-01-29 07:35:51 +0100

    remove pyright

--- lua/config/mason_lspconfig_config.lua
+++ lua/config/mason_lspconfig_config.lua
@@ -1,5 +1,5 @@
 return {
 	-- only lsp server go here, formatter are handled by conform and linter by nvim-linter
 	-- this plugin uses the lspconfig server names in the APIs it exposes - not mason.nvim package names
-	ensure_installed = { "lua_ls", "pyright", "clangd", "html", "cssls" },
+	ensure_installed = { "lua_ls", "clangd", "html", "cssls" },
 }
